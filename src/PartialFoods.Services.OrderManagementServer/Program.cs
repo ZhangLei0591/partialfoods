e7596741d7dae837f61aa88cc44afe1e30166678
autodidaddict <alothien@gmail.com>
2017-10-07 15:21:21 -0400

    Order management server now subscribes to Kafka topic and stores orders in a Postgres DB via EF.

--- src/PartialFoods.Services.OrderManagementServer/Program.cs
+++ src/PartialFoods.Services.OrderManagementServer/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using System.Linq;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System.Text;
+using PartialFoods.Services.OrderManagementServer.Entities;
 
 namespace PartialFoods.Services.OrderManagementServer
 {
@@ -6,7 +14,34 @@ namespace PartialFoods.Services.OrderManagementServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string brokerList = "localhost:9092";
+            const string topic = "orders";
+
+            var config = new Dictionary<string, object>
+            {
+                { "group.id", "order-management" },
+                { "enable.auto.commit", false },
+                { "bootstrap.servers", brokerList }
+            };
+            var context = new OrdersContext();
+            var repo = new OrderRepository(context);
+            var eventProcessor = new OrderAcceptedEventProcessor(repo);
+            var kafkaConsumer = new KafkaOrdersConsumer(topic, config, eventProcessor);
+            kafkaConsumer.Consume();
+            const int Port = 3001;
+
+            Server server = new Server
+            {
+                Services = { OrderManagement.BindService(new OrderManagementImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            Console.WriteLine("Order management RPC service listening on port " + Port);
+            Console.WriteLine("Press any key to stop");
+
+
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
         }
     }
 }
